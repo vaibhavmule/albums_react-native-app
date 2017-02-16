c08831c1d9aad89e90870ff9af9c9cee31b6b9e8
Vaibahv Mule <vaibhavmule135@gmail.com>
2017-02-17 03:24:26 +0530

    Initail Commit Header Component

---
+++ index.android.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import Header from './src/components/header';
+
+const albums = () => (
+  <Header headerText={'Albums'} />
+);
+
+AppRegistry.registerComponent('albums', () => albums);
---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+	const { textStyle, viewStyle } = styles;
+	return (
+		<View style={viewStyle}>
+			<Text style={textStyle}>{props.headerText}</Text>
+		</View>
+	)	
+}
+
+const styles = {
+	viewStyle: {
+		backgroundColor: 'skyblue',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+	},
+  textStyle: {
+    fontSize: 20
+  }
+}
+
+export default Header;
