cb385282090dc7a669f1b5a15e47887c35330e83
Vaibahv Mule <vaibhavmule135@gmail.com>
2017-02-20 22:50:31 +0530

    wip

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+import AlbumDetail from './AlbumDetail'
+
 class AlbumList extends Component {
 
 	state = { albums: []};
@@ -12,7 +14,8 @@ class AlbumList extends Component {
 	}
 
 	renderAlbums() {
-		return this.state.albums.map(albums => <Text>{albums.title}</Text>);
+		return this.state.albums.map(album => 
+			<AlbumDetail key={album.title} album={album}/>)
 	}
 
 	render() {
