6ae54d8650c0c4fd05c6af1d248941b9968f6401
Vaibahv Mule <vaibhavmule135@gmail.com>
2017-02-22 01:32:32 +0530

    add few components

--- index.android.js
+++ index.android.js
@@ -16,7 +16,7 @@ import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList'
 
 const albums = () => (
-  <View>
+  <View style={{ flex: 1}}>
     <Header headerText={'Albums'} />
     <AlbumList />
   </View>
